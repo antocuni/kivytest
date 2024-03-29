7e02884a270166e84bc7de01b2e0f2fc570278ec
NicoloVizzini <nicolovizzini2001@gmail.com>
2024-03-29 18:08:14 +0100

    Revert "added buildozer.spec"
    This reverts commit 3796314259efe7b165becdbb45e40c8e38ca6a08.

--- launcher/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/mcont2/obj/local/arm64-v8a/objs-debug/SDL2_mixer/src/codecs/music_ogg.o.d
+++
@@ -1,2 +0,0 @@
-/home/kiwi/test3/kivy_android_builder/launcher/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/SDL2_mixer/src/codecs/music_ogg.o: \
-  /home/kiwi/test3/kivy_android_builder/launcher/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_mixer/src/codecs/music_ogg.c
